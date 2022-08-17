933e33fa4f24ec7a35e778f598ebdb928849c3a4
gizemfcc <gizemficicioglu98@gmail.com>
2022-08-17 15:52:14 +0300

    kafka refactoring

---
+++ kafka/src/main/java/com/i2i/internship/eyeCell/exampleKafka/ProducerTest.java
@@ -0,0 +1,21 @@
+package com.i2i.internship.eyeCell.exampleKafka;
+import com.i2i.internship.eyeCell.kafka.method.ProducerMethod;
+import com.i2i.internship.eyeCell.kafka.modul.UsageMessage;
+
+import java.util.concurrent.ExecutionException;
+
+public class ProducerTest {
+    public static void main(String[] args) {
+        UsageMessage msg = new UsageMessage();
+        msg.setUsedService("Voice");
+        msg.setUsedAmount(5);
+        msg.setMsisdn("5350857286");
+        try {
+            ProducerMethod.runProducer(msg);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
